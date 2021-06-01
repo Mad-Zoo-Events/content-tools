490af596e7a2e0794ecdfcaca85cbfba783b9308
Adrian Riedel <adrian.riedel@outlook.com>
2021-06-02 01:02:58 +0200

    extract serveritem converter to dedicated file

--- src/drinks/generate.js
+++ src/drinks/generate.js
@@ -1,8 +1,6 @@
-import convert from "color-convert";
 import fs from "fs";
 import yaml from "js-yaml";
-
-const ACCENT_COLOR = "aqua";
+import toServerItem from "./toServerItem.js";
 
 const run = () => {
     const {drinks, shots} = load();
@@ -22,46 +20,6 @@ const run = () => {
     save(serverItems);
 
     console.log(`Generated ${drinks.length} drinks and ${shots.length} shots`);
-};
-
-const toServerItem = (item, isShot) => {
-    const displayName = isShot
-        ? `{"extra":[{"color":"white","text":"${item.name}"},{"italic":false,"color":"gray","text":" (shot)"}],"text":""}`
-        : `{"extra":[{"color":"white","text":"${item.name}"}],"text":""}`;
-
-    const lore = item.lore.map((line, index) => {
-        const textColor = index === 0
-            ? ACCENT_COLOR
-            : "dark_gray";
-
-        return `{"extra":[{"color":"${textColor}","text":"${line}"}],"text":""}`;
-    });
-
-    const potionColor = convert.hex.rgb(item.color);
-
-    return {
-        item: {
-            "==": "org.bukkit.inventory.ItemStack",
-            v: 2586,
-            type: isShot
-                ? "LINGERING_POTION"
-                : "POTION",
-            meta: {
-                "==": "ItemMeta",
-                "meta-type": "POTION",
-                "display-name": displayName,
-                lore,
-                ItemFlags: ["HIDE_ENCHANTS", "HIDE_ATTRIBUTES", "HIDE_UNBREAKABLE", "HIDE_DESTROYS", "HIDE_PLACED_ON", "HIDE_POTION_EFFECTS"],
-                "potion-type": "minecraft:water",
-                "custom-color": {
-                    "==": "Color",
-                    RED: potionColor[0],
-                    GREEN: potionColor[1],
-                    BLUE: potionColor[2]
-                }
-            }
-        }
-    };
 };
 
 const load = () => {
